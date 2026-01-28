d5ee95d1cfd7254485fb4903624fdfc102f4f001
IgorAugusto01 <augustoigor731@gmail.com>
2026-01-27 23:33:53 -0300

    TiporestauranteViewModel adicionado e implementado. TiporestauranteController modificado, TiporestauranteService modificado, ITiporestauranteService modificado. Arquivos DTO excluídos

--- Codigo/Restaurante/Service/TiporestauranteService.cs
+++ Codigo/Restaurante/Service/TiporestauranteService.cs
@@ -17,81 +17,70 @@ namespace Service
         {
             _context = context;
 		}
-		public bool Editar(uint id, string nome)
-		{
-			var tipoRestaurante = _context.Tiporestaurantes.Find(id);
-
-			if (tipoRestaurante == null) return false;
-
-			tipoRestaurante.Nome = nome;
-			_context.SaveChanges();
-
-			return true;
-		}
 
 
-		public uint Inserir(Tiporestaurante tipoRestaurante)
+        /// <summary>
+        /// Cria um novo tipo de restaurante no banco de dados
+        /// </summary>
+        /// <returns>id gerado para o tipoRestaurante</returns>
+
+        public uint Create(Tiporestaurante tipoRestaurante)
         {
             _context.Tiporestaurantes.Add(tipoRestaurante);
             _context.SaveChanges();
-            return  tipoRestaurante.Id;
-
-
-		}
-
-        public TiporestauranteDTO? Obter(uint id)
-        {
-            var tiporestaurante = _context.Tiporestaurantes.Find(id);
-
-            return (tiporestaurante == null) ? null : new TiporestauranteDTO
-            {
-                Id = tiporestaurante.Id,
-                Nome = tiporestaurante.Nome
-            };
+            return tipoRestaurante.Id;
             
         }
 
-        public IEnumerable<TiporestauranteDTO> ObterTodos()
-        {
-            return _context.Tiporestaurantes.OrderBy(tr=>tr.Nome).Select(tr => new TiporestauranteDTO
-            {
-                Id = tr.Id,
-                Nome = tr.Nome
-            }).ToList();
-            
-		}
+        /// <summary>
+        /// Remove um tipo de restaurante do banco de dados
+        /// </summary>
+        
 
-        public IEnumerable<RestaurantePorTipoDTO>? ObterTodosRestaurantesPeloId(uint id)
-        {
-			var tipoexiste = _context.Tiporestaurantes.Any(tr => tr.Id == id);
-
-			return (!tipoexiste) ? null :
-                _context.Restaurantes
-				.Where(r => r.IdTipoRestaurante == id)
-				.OrderBy(r => r.Nome)
-				.Select(r => new RestaurantePorTipoDTO
-				{
-					Id = r.Id,
-					Nome = r.Nome,
-					Cidade = r.Cidade,
-					Estado = r.Estado,
-					Telefone = r.Telefone1
-				})
-				.ToList();
-		}
-
-        public bool Remover(uint id)
+        public void Delete(uint id)
         {
             var tipoRestaurante = _context.Tiporestaurantes.Find(id);
+            if (tipoRestaurante != null)
+            {
+                _context.Tiporestaurantes.Remove(tipoRestaurante);
+                _context.SaveChanges();
+            }
+        }
 
-            if (tipoRestaurante == null) return false;
-            
-            _context.Tiporestaurantes.Remove(tipoRestaurante);
-            _context.SaveChanges();
-            return true;
-            
-		}
+        /// <summary>
+        /// Atualiza um tipo de restaurante no banco de dados
+        /// </summary>
+       
+
+        public void Edit(Tiporestaurante tipoRestaurante)
+        {
+            var tiporestaurante = _context.Tiporestaurantes.Find(tipoRestaurante.Id);
+            if (tiporestaurante != null)
+            {
+                _context.Tiporestaurantes.Update(tipoRestaurante);
+                _context.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// Obtém um tipo de restaurante pelo seu ID
+        /// </summary>
+        
+        public Tiporestaurante? Get(uint id)
+        {
+            var tipoRestaurante = _context.Tiporestaurantes.Find(id);
+            return tipoRestaurante;
+        }
+
+        /// <summary>
+        /// Obtém todos os tipos de restaurante
+        /// </summary>
+        public IEnumerable<Tiporestaurante> GetAll()
+        {
+            return _context.Tiporestaurantes.AsNoTracking();
+        }
     }
 }
+
 
 
