1dce999c8a7f8b270f0f59649758f3175e65a63b
Gabriel Silva <gabrielsilva10000@live.com>
2026-02-04 23:38:04 -0300

    implementando a cs06

---
+++ Codigo/Restaurante/RestauranteWeb/Mappers/FuncionarioProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Core.DTO;
+using RestauranteWeb.Models;
+
+namespace RestauranteWeb.Mappers
+{
+    public class FuncionarioProfile : Profile
+    {
+        public FuncionarioProfile()
+        {
+            CreateMap<FuncionarioDTO, FuncionarioViewModel>().ReverseMap();
+        }
+    }
+}
