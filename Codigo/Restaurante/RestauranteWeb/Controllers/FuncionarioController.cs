3c308bb09f45e304159daaded3298e30850353a9
joaoCarlosDev <joaocarlosprocarvalho@gmail.com>
2026-02-26 00:01:25 -0300

    grupo cardapio estilo

--- Codigo/Restaurante/RestauranteWeb/Controllers/FuncionarioController.cs
+++ Codigo/Restaurante/RestauranteWeb/Controllers/FuncionarioController.cs
@@ -1,9 +1,11 @@
 ﻿using AutoMapper;
 using Core.DTO;
 using Core.Service;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using RestauranteWeb.Models;
+using Service;
 
 namespace RestauranteWeb.Controllers
 {
@@ -52,6 +54,13 @@ namespace RestauranteWeb.Controllers
             }
             viewModel.ListaFuncoes = new SelectList(_funcaoService.GetAll(), "Id", "Nome", viewModel.IdFuncaoFuncionario);
             viewModel.ListaRestaurantes = new SelectList(_restauranteService.GetAll(), "Id", "Nome", viewModel.IdRestaurante);
+            return View(viewModel);
+        }
+
+        public ActionResult Details(uint id)
+        {
+            var funcionario = _funcionarioService.Get(id);
+            var viewModel = _mapper.Map<FuncionarioViewModel>(funcionario);
             return View(viewModel);
         }
 
