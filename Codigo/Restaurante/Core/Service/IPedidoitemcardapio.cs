51dfbe801b4dbadec14c4dffb86eb69a1de74c33
joaoCarlosDev <joaocarlosprocarvalho@gmail.com>
2026-02-26 18:46:13 -0300

    agora vamos ver se cria o banco

---
+++ Codigo/Restaurante/Core/Service/IPedidoitemcardapio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IPedidoitemcardapio
+    {
+            uint Create(Pedidoitemcardapio pedidoitemcardapio);
+    
+            void Edit(Pedidoitemcardapio pedidoitemcardapio);
+    
+            void Delete(uint id);
+    
+            Pedidoitemcardapio? Get(uint id);
+    
+            IEnumerable<Pedidoitemcardapio> GetAll();
+    }
+}
---
+++ Codigo/Restaurante/Service/PedidoitemcardapioService.cs
@@ -0,0 +1,38 @@
+﻿using Core;
+using Core.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    internal class PedidoitemcardapioService : IPedidoitemcardapio
+    {
+        public uint Create(Pedidoitemcardapio pedidoitemcardapio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(Pedidoitemcardapio pedidoitemcardapio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(uint id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pedidoitemcardapio? Get(uint id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Pedidoitemcardapio> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
