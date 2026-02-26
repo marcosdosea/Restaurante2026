150f91cc703f47c310c1a00d2ec7b98abd9f7257
IgorAugusto01 <augustoigor731@gmail.com>
2026-02-25 21:20:58 -0300

    Alteracoes feitas

--- Codigo/Restaurante/Core/DTO/PedidoDTO.cs
+++ Codigo/Restaurante/Core/DTO/PedidoDTO.cs
@@ -2,26 +2,34 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 namespace Core.DTO
 {
     public class PedidoDTO
     {
-    public uint Id { get; set; }
+        [Key]
+        [Display(Name = "ID do Pedido")]
+        public uint Id { get; set; }
 
-    public DateTime DataHoraSolicitacao { get; set; }
+        [Display(Name = "Data e Hora da Solicitação")]
+        [Required(ErrorMessage = "A data e hora da solicitação são obrigatórias")]
+        public DateTime DataHoraSolicitacao { get; set; }
 
-    public DateTime DaaHoraAtendimento { get; set; }
+        [Display(Name = "Data e Hora do Atendimento")]
+        [Required(ErrorMessage = "A data e hora do atendimento são obrigatórias")]
+        public DateTime DaaHoraAtendimento { get; set; }
 
-    public uint IdAtendimento { get; set; }
+        [Display(Name = "Atendimento")]
+        [Required(ErrorMessage = "O atendimento é obrigatório")]
+        public uint IdAtendimento { get; set; }
 
-    public uint IdGarcom { get; set; }
+        [Display(Name = "Garçom")]
+        [Required(ErrorMessage = "O garçom é obrigatório")]
+        public uint IdGarcom { get; set; }
 
-    /// <summary>
-    /// S - SOLICITADO
-    /// C - CANCELADO
-    /// A - ATENDIDO
-    /// </summary>
-    public string Status { get; set; } = null!;
+        [Display(Name = "Status do Pedido")]
+        [Required(ErrorMessage = "O status do pedido é obrigatório")]
+        public string Status { get; set; } = null!;
     }
 }
